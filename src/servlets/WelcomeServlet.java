2983dc185f6681343ab559b092f1e221e2e5b9eb
yzx <2268244197@qq.com>
2020-11-23 12:13:39 +0800

    创建分支

--- src/servlets/WelcomeServlet.java
+++ src/servlets/WelcomeServlet.java
@@ -17,7 +17,7 @@ public class WelcomeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=gb2312");//设置中文编码
         PrintWriter out = response.getWriter();//获得out对象
-        out.println("欢迎来到本系统！");
+        out.println("欢迎来到本系统22！");
     }
 //    public static void main(String[] ...arc){
 //        System.out.println("das");
